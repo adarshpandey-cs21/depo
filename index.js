f5a6ef513a7e51e49f289bb0dc0b6b99e7a22699
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-09-27 16:21:12 +0530

    s

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ let comments=[
 ]
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use('/',express.static(path.join(__dirname,'public')));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
