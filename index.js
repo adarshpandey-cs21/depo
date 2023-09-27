268ca609cdd61333eee1317f9fb6e5ee59ba4f33
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-09-27 14:41:08 +0530

    Update index.js
    fds

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.get('/',(req,res)=>{
+app.get('/depo/',(req,res)=>{
     res.send("hello");
 });
 
